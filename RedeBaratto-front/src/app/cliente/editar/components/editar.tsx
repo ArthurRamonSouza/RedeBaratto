00f0d324c10a1c875d31a35f600a5c0ccade342f
Arthur Ramon Souza <arthuramon.souza93@hotmail.com>
2024-04-26 08:31:56 -0300

    Corrigindo e adicionando mapeamento para API na tela de editar cliente.

--- RedeBaratto-front/src/app/cliente/editar/components/editar.tsx
+++ RedeBaratto-front/src/app/cliente/editar/components/editar.tsx
@@ -1,10 +1,9 @@
 'use client';
-import React, { useState } from 'react';
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
-import { Checkbox } from "@/components/ui/checkbox"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import {Label} from "@/components/ui/label"
+import {Input} from "@/components/ui/input"
+import {Checkbox} from "@/components/ui/checkbox"
+import {Button} from "@/components/ui/button"
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card"
 
 import axios from 'axios'
 
@@ -22,7 +21,7 @@ interface FormDataValues {
     sousense: boolean;
 }
 
-export default function Cadastro() {
+export default function EditarCliente() {
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault(); // Prevent default form submission behavior
 
@@ -37,9 +36,8 @@ export default function Cadastro() {
             sousense: formData.has('sousaParaiba') as boolean
         };
 
-        await http.post('cadastrar', data);
-
-        console.log(JSON.stringify(data, null, 2));
+        await http.put(`atualizar/${data.cpf}`, data);
+        window.location.href = '/cliente/pedidos';
     };
 
     return (
@@ -47,43 +45,45 @@ export default function Cadastro() {
             <form onSubmit={handleSubmit}>
                 <Card className="mx-auto max-w-sm space-y-6 shadow-lg">
                     <CardHeader className="space-y-2 text-center">
-                        <CardTitle className="text-3xl font-bold">Cadastrar</CardTitle>
-                        <CardDescription className="text-gray-500 dark:text-gray-400">Insira suas informações para criar a conta</CardDescription>
+                        <CardTitle className="text-3xl font-bold">Editar</CardTitle>
+                        <CardDescription className="text-gray-500 dark:text-gray-400">Insira suas informações para
+                            atualizar a conta</CardDescription>
                     </CardHeader>
                     <CardContent className="space-y-4">
                         <div className="grid grid-cols-2 gap-4">
                             <div className="space-y-2">
                                 <Label htmlFor="first-name">Primeiro nome</Label>
-                                <Input name="firstName" id="first-name" placeholder="Arthur" required />
+                                <Input name="firstName" id="first-name" placeholder="Arthur" required/>
                             </div>
                             <div className="space-y-2">
                                 <Label htmlFor="last-name">Último nome</Label>
-                                <Input name="lastName" id="last-name" placeholder="Ramón" required />
+                                <Input name="lastName" id="last-name" placeholder="Ramón" required/>
                             </div>
                         </div>
                         <div className="space-y-2">
                             <Label htmlFor="cpf">CPF</Label>
-                            <Input name="cpf" id="cpf" placeholder="123.456.789-00" required />
+                            <Input name="cpf" id="cpf" placeholder="123.456.789-00" required/>
                         </div>
                         <div className="space-y-2">
                             <Label htmlFor="password">Senha</Label>
-                            <Input name="password" id="password" required type="password" />
+                            <Input name="password" id="password" required type="password"/>
                         </div>
                         <div className="space-y-2">
                             <div className="flex items-center space-x-2">
-                                <Checkbox id="flamengo-fan" name="flamengoFan" />
+                                <Checkbox id="flamengo-fan" name="flamengoFan"/>
                                 <Label htmlFor="flamengo-fan">Sou mengão</Label>
                             </div>
                             <div className="flex items-center space-x-2">
-                                <Checkbox id="one-piece-fan" name="onePieceFan" />
+                                <Checkbox id="one-piece-fan" name="onePieceFan"/>
                                 <Label htmlFor="one-piece-fan">Sou fã de One Piece</Label>
                             </div>
                             <div className="flex items-center space-x-2">
-                                <Checkbox id="sousa-paraiba" name="sousaParaiba" />
+                                <Checkbox id="sousa-paraiba" name="sousaParaiba"/>
                                 <Label htmlFor="sousa-paraiba">Sou sousense</Label>
                             </div>
                         </div>
-                        <Button className="w-full" type="submit" href={"/login"}>Cadastrar</Button>
+                        <Button className="w-full" type="submit">Editar</Button>
+                        <Button className="w-full" onClick={() => window.location.href = "/cliente/pedidos"}>Voltar</Button>
                     </CardContent>
                 </Card>
             </form>
